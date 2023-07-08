1bdd521600ae0cbed4426fafd2967008df78ae55
Kenan Muradov <maljerov.1999@gmail.com>
2023-07-08 21:59:20 +0300

    Redirect Change

--- MyMiddleWebServer/ActionResults/RedirectResult.cs
+++ MyMiddleWebServer/ActionResults/RedirectResult.cs
@@ -11,6 +11,6 @@ public class RedirectResult : IActionResult
 {
     public void ExecuteResult(HttpListenerContext context)
     {
-        context.Response.Redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        context.Response.Redirect("https://duiqt.github.io/herta_kuru/");
     }
 }
